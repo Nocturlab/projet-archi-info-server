1877669193955d7757f3cb77867296837ce5d489
Flavien CADET <flavien.cadet@nocturlab.fr>
2019-10-10 22:19:21 +0200

    Fix du repository Parking
    J'ai oublié l'importation de l'annotation avant de l'utiliser (c'est ça de dev sur mobile et de pas pouvoir tester)

--- src/main/java/fr/nocturlab/repository/ParkingRepository.java
+++ src/main/java/fr/nocturlab/repository/ParkingRepository.java
@@ -1,7 +1,9 @@
 package fr.nocturlab.repository;
 
 import fr.nocturlab.model.Parking;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ParkingRepository extends PagingAndSortingRepository<Parking, Integer> {
