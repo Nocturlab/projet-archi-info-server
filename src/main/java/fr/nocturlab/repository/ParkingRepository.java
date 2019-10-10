a6984a7d335d44c286333c62fc57f4977ce1e9aa
Flavien CADET <flavien.cadet@nocturlab.fr>
2019-10-10 22:14:17 +0200

    Fix du repository Parking
    Il manquait l'annotation @RepositoryRestResource pour le rendre accessible.

--- src/main/java/fr/nocturlab/repository/ParkingRepository.java
+++ src/main/java/fr/nocturlab/repository/ParkingRepository.java
@@ -3,7 +3,7 @@ package fr.nocturlab.repository;
 import fr.nocturlab.model.Parking;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-
+@RepositoryRestResource
 public interface ParkingRepository extends PagingAndSortingRepository<Parking, Integer> {
 
     Parking findByPlacesDisponiblesIsGreaterThan(Integer x);
