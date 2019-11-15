be0530ff32a381b1f64308bbac32f97ddf38c9fc
Bastien <bastien.fournier5@gmail.com>
2019-11-15 08:52:52 +0100

    route parkings/all -> parkings temps reel et stationnements

--- src/main/java/fr/nocturlab/controller/ParkingController.java
+++ src/main/java/fr/nocturlab/controller/ParkingController.java
@@ -28,20 +28,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-
-import fr.nocturlab.model.Parking;
-import fr.nocturlab.repository.ParkingRepository;
-
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 
 @RestController
 @RequestMapping("/parkings")
