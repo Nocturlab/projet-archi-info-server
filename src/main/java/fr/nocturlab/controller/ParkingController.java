2083d31b58355506b5d37164155e99a4197f9c7b
alexis <alexis969@live.fr>
2019-11-15 11:13:35 +0100

    Correction sur le calcule de la distance

--- src/main/java/fr/nocturlab/controller/ParkingController.java
+++ src/main/java/fr/nocturlab/controller/ParkingController.java
@@ -214,17 +214,9 @@ public class ParkingController {
         return array.toString();
     }
 
-    private float distance(double lat1, double lng1, double lat2, double lng2) {
-        double earthRadius = 6371000; //meters
-        double dLat = Math.toRadians(lat2-lat1);
-        double dLng = Math.toRadians(lng2-lng1);
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-                Math.sin(dLng/2) * Math.sin(dLng/2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        float dist = (float) (earthRadius * c);
+    private double distance(double lat1, double lng1, double lat2, double lng2) {
 
-        return dist;
+        return Math.sqrt(Math.pow(lat1-lat2, 2) + Math.pow(lng1-lng2, 2));
     }
 
     /**
